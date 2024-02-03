34d5e5ceb6cac86c8dd1f8744641c91217b79663
Mahdi Masaeli <masaelimahdiwork@gmail.com>
2024-02-03 13:02:09 +0330

    add search animation and MB and px phrase

---
+++ content/collections/documentry/mstnd-tsty.md
@@ -0,0 +1,18 @@
+---
+id: 0f32eb12-f054-4b72-af43-4483a84ee479
+blueprint: documentry
+title: 'مستند تستی'
+subtitle: false
+documentaryDownload:
+  -
+    id: ls5u1miv
+    quality: ۱۰۸۰
+    docLink: cover.jpg
+    downloadSize: ۵۰۰
+    DocInfo: 'زبان اصلی'
+    type: docDownloadInfo
+    enabled: true
+starContent: false
+updated_by: c1ede7bd-11c2-4af0-9224-2178b8f71939
+updated_at: 1706950166
+---
--- resources/js/search-bar.js
+++ resources/js/search-bar.js
@@ -1,11 +1,11 @@
-window.toggleSearchElement = function toggleSearchElement(){
-    
-    let searchElement = document.getElementById('searchelement');
-    let searchElementStyle = getComputedStyle(searchElement)
-    
-    if (searchElementStyle.display === 'none') {
-      searchElement.style.display = 'flex'
-    } else {
-      searchElement.style.display = 'none';
-    }
-};
+var acc = document.getElementById("search-button");
+
+acc.addEventListener("click", function() {
+  var searchPanel = document.getElementById('search-panel');
+  searchPanel.classList.toggle("active");
+  if (searchPanel.style.maxHeight) {
+    searchPanel.style.maxHeight = null;
+  } else {
+    searchPanel.style.maxHeight =50 + "px";
+  }
+});
